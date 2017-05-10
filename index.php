0435d3870470752e6d84273c3bc5a653044ab9e1
Ramgopal Bhat <subramanyaramgopal@gmail.com>
2017-05-10 18:41:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit'])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <!-- js -->
-    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.min.js"></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.min.js"></script>
 
   </head>
   <body>
